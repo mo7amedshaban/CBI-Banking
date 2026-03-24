d198f4ca1063cd4a157287c1b43d25732b01dfe2
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2026-03-24 16:52:26 +0200

    Add Report Section

---
+++ UsrCBI/Schemas/UsrReport_Detail/UsrReport_Detail.js
@@ -0,0 +1,8 @@
+define('UsrReport_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrReport',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
