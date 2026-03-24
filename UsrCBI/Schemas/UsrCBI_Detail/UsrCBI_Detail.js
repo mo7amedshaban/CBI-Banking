44236f8f3e43be0b92645f742e0e9b3c14eb2de1
unknown <Administrator@CODE81-M69SS.Ghobash.local>
2026-03-24 13:22:15 +0200

    Add ReadMe

---
+++ UsrCBI/Schemas/UsrCBI_Detail/UsrCBI_Detail.js
@@ -0,0 +1,8 @@
+define('UsrCBI_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrCBI',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
